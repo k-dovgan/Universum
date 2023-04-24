8777c55fb477c040a893add0cebc2db13520e647
Kateryna Dovgan <46348880+k-dovgan@users.noreply.github.com>
2023-04-24 16:03:33 +0300

    Update github_actions_vcs.py

--- universum/modules/vcs/github_actions_vcs.py
+++ universum/modules/vcs/github_actions_vcs.py
@@ -22,7 +22,7 @@ class GithubActionsMainVcs(ReportObserver, git_vcs.GitMainVcs):
         parser = argument_parser.get_or_create_group("GitHub Actions", "GitHub repository settings for GH Actions")
 
         parser.add_argument("--ghactions-token", "-ght", dest="token", metavar="GITHUB_TOKEN",
-                            help="Is stored in ${{ secrets.GITHUB_TOKEN }}")
+                        help="Is stored in ${{ secrets.GITHUB_TOKEN }}")
         parser.add_argument("--ghactions-payload", "-ghp", dest="payload", metavar="GITHUB_PAYLOAD",
                             help="File path: ${{ github.event_path }}")
 
